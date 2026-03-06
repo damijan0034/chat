b80f1d427b4a03b34bf743a51bdc08349068c9e1
Goran <damijan0034@gmail.com>
2026-03-06 19:47:11 +0100

    jwt

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken"
+
+export const generateTokenAndSetCookie=async(userId,res)=>{
+    try {
+        const token=jwt.sign({userId},process.env.JWT_SECRET,{
+            expiresIn:"30d"
+        })
+
+        res.cookie("token",token,{
+            httpOnly: true,
+            maxAge: 30 * 24 * 60 * 60 * 1000 ,// 30 days
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+
+        })
+
+    } catch (error) {
+        throw new Error("Error generating token")
+    }
+}
+
+
