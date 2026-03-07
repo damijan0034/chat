1559515d2a9f5914d08b0df5234ea49272641523
Goran <damijan0034@gmail.com>
2026-03-07 09:19:02 +0100

    conversation model

---
+++ backend/middlewares/checkUser.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model.js"
+
+export const checkUser=async(req,res,next)=>{
+    try {
+        const token=req.cookies.token
+        if(!token){
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+        if(!decoded){
+
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+        const user=await User.findById(decoded.userId)
+        if(!user){
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+        req.user=user
+        next()
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { setUser,loginUser,logoutUser } from "../controllers/user.controller.js";
+import { checkUser } from "../middlewares/checkUser.js";
 const router = express.Router();
 
 router.post("/register",setUser)
-router.post("/login",loginUser)
+router.post("/login",checkUser,loginUser)
 router.post("/logout",logoutUser)
 
 export default router;
