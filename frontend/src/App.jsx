260de41ffa00da6f3d86ca4c330f5974f04a5d5e
Goran <damijan0034@gmail.com>
2026-03-09 12:21:34 +0100

    tailwind and daisyui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-red-500">Hello, Tailwind!</div>
+      <button className="btn btn-primary">Click me</button>
+    </>
+  
+  )
+}
+
+export default App
