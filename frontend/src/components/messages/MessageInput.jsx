a6052c8926378ad832adafcaba4dbcb68da0f0d5
Goran <damijan0034@gmail.com>
2026-03-11 20:16:47 +0100

    Frontend completed

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -3,7 +3,7 @@ import {BsSend} from 'react-icons/bs'
 const MessageInput = () => {
   return (
     <form  className="my-3 px-4">
-        <div className="w-full">
+        <div className="relative w-full">
             <input placeholder='Send a message'
              type="text" className="block bg-gray-700 p-2.5 border border border-gray-600 rounded-lg w-full text-white text-sm" />
              <button type='submit' className='absolute inset-y-0 flex items-center pe-3 end-0'>
